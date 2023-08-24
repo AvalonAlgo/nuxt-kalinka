14dff949f807a088c7fa9579ac8af0eef7a4d861
dekesk <dekesk@ttu.ee>
2023-08-24 15:55:54 +0300

    45

--- server/api/send-contact-email.post.ts
+++ server/api/send-contact-email.post.ts
@@ -7,20 +7,20 @@ sendgrid.setApiKey("SG.0sksgNziSy2U1eRAudwgzQ.C3D5HB-sS-gViN5U5MGHGMsoR1oLH7Iv24
 export default defineEventHandler(async (event) => {
   const body = await readBody(event);
 
-  // const template = await useCompiler('contact-email.vue',
-  //   // props for email template
-  //   {
-  //     name: body.name,
-  //     email: body.email,
-  //     message: body.message,
-  //   }
-  // )
+  const template = await useCompiler('contact-email.vue',
+    // props for email template
+    {
+      name: body.name,
+      email: body.email,
+      message: body.message,
+    }
+  )
 
   const options = {
     from: 'AvalonAlgo@protonmail.com',
     to: 'denizkeskin@gmail.com',
     subject: 'Калинка - Новый вопрос',
-    html: 'template',
+    html: template,
   };
 
   const status = await sendgrid.send(options);
