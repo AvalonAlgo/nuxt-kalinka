aef3164ba96007838a2df4c278d6b099259eeaab
dekesk <dekesk@ttu.ee>
2023-08-24 16:29:23 +0300

    delete rebyonka

--- pages/lesson.vue
+++ pages/lesson.vue
@@ -131,7 +131,7 @@ const handleSubmit = async () => {
 
             <div class="p-2 w-1/2">
               <div class="relative">
-                <label for="languageLevel" class="leading-7 text-sm text-gray-600">Языковой уровень ребенка</label>
+                <label for="languageLevel" class="leading-7 text-sm text-gray-600">Языковой уровень</label>
                 <input required v-model="languageLevel" type="text" id="languageLevel" name="languageLevel" class="w-full bg-gray-100 bg-opacity-50 rounded border border-gray-300 focus:border-indigo-500 focus:bg-white focus:ring-2 focus:ring-indigo-200 text-base outline-none text-gray-700 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out">
               </div>
             </div>
