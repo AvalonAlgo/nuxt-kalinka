5a36f885914c6a361cf5e7c552612187dcdfbffc
dekesk <dekesk@ttu.ee>
2023-09-03 18:25:09 +0300

    added images and groups

--- components/our-groups.vue
+++ components/our-groups.vue
@@ -57,30 +57,26 @@ watchEffect(() => {
         </div>
 
         <div class="flex flex-wrap flex-col mx-auto w-full justify-center sm:justify-start space-y-4 bg-gradient-to-br from-violet-500 to-red-500 bg-clip-text text-transparent">
-          <div class="flex flex-col text-xl space-y-2 md:space-y-0 md:grid md:grid-cols-3 md:gap-4">
-            <UButton class="bg-gradient-to-br from-violet-500 to-red-500" label="Open" @click="isOpen = true; group = 1" />
-            <UButton class="bg-gradient-to-br from-violet-500 to-red-500" label="Open" @click="isOpen = true; group = 2" />
-            <UButton class="bg-gradient-to-br from-violet-500 to-red-500" label="Open" @click="isOpen = true; group = 3" />
-            <UButton class="bg-gradient-to-br from-violet-500 to-red-500" label="Open" @click="isOpen = true; group = 4" />
-            <UButton class="bg-gradient-to-br from-violet-500 to-red-500" label="Open" @click="isOpen = true; group = 5" />
-            <UButton class="bg-gradient-to-br from-violet-500 to-red-500" label="Open" @click="isOpen = true; group = 6" />
-            <UButton class="bg-gradient-to-br from-violet-500 to-red-500" label="Open" @click="isOpen = true; group = 7" />
-            <UButton class="bg-gradient-to-br from-violet-500 to-red-500" label="Open" @click="isOpen = true; group = 8" />
+          <div class="flex flex-col text-xl space-y-2 md:space-y-0 md:grid md:grid-cols-4 md:gap-4">
+            <UButton class="bg-gradient-to-br from-violet-500 to-red-500 justify-center" label="Малыши (2-3 года)" @click="isOpen = true; group = 1" />
+            <UButton class="bg-gradient-to-br from-violet-500 to-red-500 justify-center" label="Солнышко (3-4 года)" @click="isOpen = true; group = 2" />
+            <UButton class="bg-gradient-to-br from-violet-500 to-red-500 justify-center" label="Букварята (5-6 лет)" @click="isOpen = true; group = 3" />
+            <UButton class="bg-gradient-to-br from-violet-500 to-red-500 justify-center" label="Знайки (6-7 лет)" @click="isOpen = true; group = 4" />
+            <UButton class="bg-gradient-to-br from-violet-500 to-red-500 justify-center" label="Читайки (7-8 лет)" @click="isOpen = true; group = 5" />
+            <UButton class="bg-gradient-to-br from-violet-500 to-red-500 justify-center" label="Фантазёры (8-9 лет)" @click="isOpen = true; group = 6" />
+            <UButton class="bg-gradient-to-br from-violet-500 to-red-500 justify-center" label="Волшебники (9-11)" @click="isOpen = true; group = 7" />
+            <UButton class="bg-gradient-to-br from-violet-500 to-red-500 justify-center" label="Книгоманы (11+)" @click="isOpen = true; group = 8" />
           </div>
 
           <UModal v-if="group === 1" v-model="isOpen">
             <UCard :ui="{ divide: 'divide-y divide-gray-100 dark:divide-gray-800' }">
               
               <template #header>
-                Развивающие занятия для малышей от 2х лет
+                Малыши
               </template>
 
               Ваш малыш еще не ходит в садик, но Вы хотите, чтобы он развивался и узнавал новое в коллективе, учился взаимодействовать со сверстниками
-              <img src="https://th.bing.com/th/id/OIP.9N8kLO9PMHuCJ07SgiV-eAHaIs?pid=ImgDet&rs=1" />
-
-              <template #footer>
-                <h2>test</h2>
-              </template>
+              <NuxtImg src="/malishi.jpg" class="rounded-lg mx-auto mt-4" />
 
             </UCard>
           </UModal>
@@ -89,15 +85,11 @@ watchEffect(() => {
             <UCard :ui="{ divide: 'divide-y divide-gray-100 dark:divide-gray-800' }">
               
               <template #header>
-                Развивающие занятия для малышей от 2х лет
+                Солнышко
               </template>
 
               Ваш малыш еще не ходит в садик, но Вы хотите, чтобы он развивался и узнавал новое в коллективе, учился взаимодействовать со сверстниками
-              <img src="https://th.bing.com/th/id/OIP.9N8kLO9PMHuCJ07SgiV-eAHaIs?pid=ImgDet&rs=1" />
-
-              <template #footer>
-                <h2>test</h2>
-              </template>
+              <NuxtImg src="/solnishko.png" class="rounded-lg mx-auto mt-4" />
 
             </UCard>
           </UModal>
@@ -106,15 +98,11 @@ watchEffect(() => {
             <UCard :ui="{ divide: 'divide-y divide-gray-100 dark:divide-gray-800' }">
               
               <template #header>
-                Развивающие занятия для малышей от 2х лет
+                Букварята
               </template>
 
               Ваш малыш еще не ходит в садик, но Вы хотите, чтобы он развивался и узнавал новое в коллективе, учился взаимодействовать со сверстниками
-              <img src="https://th.bing.com/th/id/OIP.9N8kLO9PMHuCJ07SgiV-eAHaIs?pid=ImgDet&rs=1" />
-
-              <template #footer>
-                <h2>test</h2>
-              </template>
+              <NuxtImg src="/bukvaryata.jpg" class="rounded-lg mx-auto mt-4" />
 
             </UCard>
           </UModal>
@@ -123,15 +111,11 @@ watchEffect(() => {
             <UCard :ui="{ divide: 'divide-y divide-gray-100 dark:divide-gray-800' }">
               
               <template #header>
-                Развивающие занятия для малышей от 2х лет
+                Знайки
               </template>
 
               Ваш малыш еще не ходит в садик, но Вы хотите, чтобы он развивался и узнавал новое в коллективе, учился взаимодействовать со сверстниками
-              <img src="https://th.bing.com/th/id/OIP.9N8kLO9PMHuCJ07SgiV-eAHaIs?pid=ImgDet&rs=1" />
-
-              <template #footer>
-                <h2>test</h2>
-              </template>
+              <NuxtImg src="/znayki.jpg" class="rounded-lg mx-auto mt-4" />
 
             </UCard>
           </UModal>
@@ -140,15 +124,11 @@ watchEffect(() => {
             <UCard :ui="{ divide: 'divide-y divide-gray-100 dark:divide-gray-800' }">
               
               <template #header>
-                Развивающие занятия для малышей от 2х лет
+                Читайки
               </template>
 
               Ваш малыш еще не ходит в садик, но Вы хотите, чтобы он развивался и узнавал новое в коллективе, учился взаимодействовать со сверстниками
-              <img src="https://th.bing.com/th/id/OIP.9N8kLO9PMHuCJ07SgiV-eAHaIs?pid=ImgDet&rs=1" />
-
-              <template #footer>
-                <h2>test</h2>
-              </template>
+              <NuxtImg src="/chitayki.png" class="rounded-lg mx-auto mt-4" />
 
             </UCard>
           </UModal>
@@ -157,15 +137,11 @@ watchEffect(() => {
             <UCard :ui="{ divide: 'divide-y divide-gray-100 dark:divide-gray-800' }">
               
               <template #header>
-                Развивающие занятия для малышей от 2х лет
+                Фантазёры
               </template>
 
               Ваш малыш еще не ходит в садик, но Вы хотите, чтобы он развивался и узнавал новое в коллективе, учился взаимодействовать со сверстниками
-              <img src="https://th.bing.com/th/id/OIP.9N8kLO9PMHuCJ07SgiV-eAHaIs?pid=ImgDet&rs=1" />
-
-              <template #footer>
-                <h2>test</h2>
-              </template>
+              <NuxtImg src="/fantazyeri.jpg" class="rounded-lg mx-auto mt-4" />
 
             </UCard>
           </UModal>
@@ -174,15 +150,11 @@ watchEffect(() => {
             <UCard :ui="{ divide: 'divide-y divide-gray-100 dark:divide-gray-800' }">
               
               <template #header>
-                Развивающие занятия для малышей от 2х лет
+                Волшебники
               </template>
 
               Ваш малыш еще не ходит в садик, но Вы хотите, чтобы он развивался и узнавал новое в коллективе, учился взаимодействовать со сверстниками
-              <img src="https://th.bing.com/th/id/OIP.9N8kLO9PMHuCJ07SgiV-eAHaIs?pid=ImgDet&rs=1" />
-
-              <template #footer>
-                <h2>test</h2>
-              </template>
+              <NuxtImg src="/volshebniki.jpg" class="rounded-lg mx-auto mt-4" />
 
             </UCard>
           </UModal>
@@ -191,15 +163,11 @@ watchEffect(() => {
             <UCard :ui="{ divide: 'divide-y divide-gray-100 dark:divide-gray-800' }">
               
               <template #header>
-                Развивающие занятия для малышей от 2х лет
+                Книгоманы
               </template>
 
               Ваш малыш еще не ходит в садик, но Вы хотите, чтобы он развивался и узнавал новое в коллективе, учился взаимодействовать со сверстниками
-              <img src="https://th.bing.com/th/id/OIP.9N8kLO9PMHuCJ07SgiV-eAHaIs?pid=ImgDet&rs=1" />
-
-              <template #footer>
-                <h2>test</h2>
-              </template>
+              <NuxtImg src="/knigomani.jpg" class="rounded-lg mx-auto mt-4" />
 
             </UCard>
           </UModal>
