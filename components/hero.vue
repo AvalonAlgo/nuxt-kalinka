f752544cc57f3e2121d2f564ad6fda468e050910
dekesk <dekesk@ttu.ee>
2023-09-10 23:18:21 +0300

    sdsff

--- components/hero.vue
+++ components/hero.vue
@@ -11,7 +11,7 @@
           <!-- Transparent Overlay with flex utilities -->
           <div class="absolute inset-0 flex flex-col space-y-6 sm:space-y-10 items-center justify-center">
             <h1 style="font-family: Amatic SC" class="text-center text-3xl sm:text-4xl font-bold text-white">Добро пожаловать в игровую группу "Калинка"!</h1>
-            <UButton color="yellow" @click="navigateTo('/lesson')" class="text-l sm:text-xl opacity-75 hover:opacity-100" label="Записаться" />
+            <UButton color="yellow" @click="navigateTo('/lesson')" class="text-l sm:text-xl " label="Записаться" />
           </div>
         </div>
       </div>
