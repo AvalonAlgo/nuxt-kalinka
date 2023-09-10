bd08933f9d5de73a79d79a85000f1c9d0f171374
dekesk <dekesk@ttu.ee>
2023-09-10 21:23:50 +0300

    dfgdfg

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,11 @@
 module.exports = {
   content: [],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        primary: defaultTheme.colors.white
+      }
+    },
   },
   plugins: [],
 }
