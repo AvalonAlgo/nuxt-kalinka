85b6912602e554dce410b21a5ba7d8c645062414
Deniz Keskin <AvalonAlgo@protonmail.com>
2023-11-08 17:27:11 +0200

    add instagram button

--- layouts/default.vue
+++ layouts/default.vue
@@ -9,5 +9,12 @@
     <Header />
     <slot />
     <!-- <Footer /> -->
+    <a href="https://www.google.com">
+      <NuxtImg 
+        height="36" 
+        src="/icons/new-Instagram-logo-png-full-colour-glyph.png"
+        class="rounded-lg fixed bottom-0 right-0 mb-4 mr-4 p-3 bg-gray-100 border border-gray-300 shadow-lg z-50"
+      />
+    </a>
   </div>
 </template>
