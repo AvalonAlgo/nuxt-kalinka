3e799c67439996943aed7fdd55048a5b04547a08
Deniz Keskin <AvalonAlgo@protonmail.com>
2023-11-11 14:14:03 +0200

    remove instagram button

--- layouts/default.vue
+++ layouts/default.vue
@@ -9,12 +9,12 @@
     <Header />
     <slot />
     <!-- <Footer /> -->
-    <a href="https://www.google.com" class="rounded-lg fixed bottom-0 right-0 mb-4 mr-4 p-3 bg-gray-100 border border-gray-300 shadow-lg z-50">
+    <!-- <a href="https://www.google.com" class="rounded-lg fixed bottom-0 right-0 mb-4 mr-4 p-3 bg-gray-100 border border-gray-300 shadow-lg z-50">
       <NuxtImg 
         height="36" 
         src="/icons/new-Instagram-logo-png-full-colour-glyph.png"
         class="rounded-lg fixed bottom-0 right-0 mb-4 mr-4 p-3 bg-gray-100 border border-gray-300 shadow-lg z-50"
       />
-    </a>
+    </a> -->
   </div>
 </template>
