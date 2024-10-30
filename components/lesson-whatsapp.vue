d766ba5009bd03815348e180925951a178cad178
Deniz Keskin <denizkeskin@gmail.com>
2024-10-30 05:31:34 +0200

    add latest

--- components/lesson-whatsapp.vue
+++ components/lesson-whatsapp.vue
@@ -1,19 +1,23 @@
 <script setup>
-const props = defineProps ({
-  stacked: Boolean
-})
+const props = defineProps({
+  stacked: Boolean,
+});
 
-const el = ref(null)
-const isVisible = useElementVisibility(el)
+const el = ref(null);
+const isVisible = useElementVisibility(el);
 </script>
 
 <style scoped>
 @keyframes flashWhiteOrange {
-  50% { background-color: white; }
+  50% {
+    background-color: white;
+  }
 }
 
 @keyframes flashWhiteGreen {
-  50% { background-color: white; }
+  50% {
+    background-color: white;
+  }
 }
 
 .flash-bg-orange {
@@ -30,57 +34,36 @@ const isVisible = useElementVisibility(el)
     <div
       ref="el"
       :class="{
-        'flex-col space-y-4' : props.stacked,
-        'flex-row' : !props.stacked,
-        'flex items-center justify-center px-5 py-12' : true,
+        'flex-col space-y-4': props.stacked,
+        'flex-row': !props.stacked,
+        'flex items-center justify-center px-5 py-12': true,
       }"
       class="flex px-5 py-6"
     >
-      <NuxtLink 
-          to="/lesson" 
-          :class="{
-            'flash-bg-orange': isVisible, 
-            'w-2/3': props.stacked, 
-            'w-1/2': !props.stacked,
-            'inline-flex' : !props.stacked,
-            'text-center font-semibold': true,
-            'justify-center': true,
-            'text-white': true,
-            'bg-orange-500': true,
-            'border-0': true,
-            'py-2': true,
-            'px-6': true,
-            'focus:outline-none': true,
-            'hover:bg-orange-600': true,
-            'rounded-xl': true
-          }">
-        Записаться на
-        <br /> бесплатное занятие
-      </NuxtLink>
-
       <a
-        href="https://wa.me/905388895268" 
+        href="https://wa.me/905388895268"
         :class="{
-            'flash-bg-green': isVisible, 
-            'w-2/3': props.stacked, 
-            'w-1/2': !props.stacked,
-            'inline-flex' : !props.stacked,
-            'ml-4' : !props.stacked,
-            'text-center font-semibold': true,
-            'justify-center': true,
-            'text-white': true,
-            'bg-green-500': true,
-            'border-0': true,
-            'py-2': true,
-            'px-6': true,
-            'focus:outline-none': true,
-            'hover:bg-green-600': true,
-            'rounded-xl': true
-          }">
+          'flash-bg-green': isVisible,
+          'w-2/3': props.stacked,
+          'w-1/2': !props.stacked,
+          'inline-flex': !props.stacked,
+          'ml-4': !props.stacked,
+          'text-center font-semibold': true,
+          'justify-center': true,
+          'text-white': true,
+          'bg-green-500': true,
+          'border-0': true,
+          'py-2': true,
+          'px-6': true,
+          'focus:outline-none': true,
+          'hover:bg-green-600': true,
+          'rounded-xl': true,
+        }"
+      >
         Задать вопросы
-        <br /> по Whatsapp
+        <br />
+        по Whatsapp
       </a>
-  
     </div>
-</div>
+  </div>
 </template>
